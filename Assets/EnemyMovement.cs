85af8c521ae6a23801f2be0eb77a11c109670039
DESKTOP-REUIVED\royba <roybal.usmc@gmail.com>
2018-08-26 18:18:57 -0700

    Added co routine to move enemy to the finish position and print the coordinates of where the enemy currently is.

--- Assets/EnemyMovement.cs
+++ Assets/EnemyMovement.cs
@@ -5,7 +5,28 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
 
-    [SerializeField] List<Cubes> path;
+    [SerializeField] List<Waypoint> path;
     
-	
+    void Start()
+    {
+        StartCoroutine(PrintAllWaypoints());  //must use StartCoroutine() in order to call methods / coroutines
+    }
+
+	void Update()
+    {
+
+    }
+
+    IEnumerator PrintAllWaypoints()  //IEnumerator returns a value and creates a co-routine
+    {
+        print("Starting Patrol");
+        foreach (Waypoint cubeWaypoint in path)
+        {
+            transform.position = cubeWaypoint.transform.position;
+            print("Visiting Block: " + cubeWaypoint.name);
+            yield return new WaitForSeconds(1f);
+        }
+        print("Ending Patrol");
+    }
+
 }
