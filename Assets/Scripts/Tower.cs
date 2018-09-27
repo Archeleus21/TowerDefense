b3fdc109ef56c776b1214dfd04e26560daaae280
Roybal_A <Roybal.usmc@gmail.com>
2018-09-26 19:36:18 -0700

    added voxel models, created terrain, added test turrets and one enemy, turrets lookat target

---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+
+    [SerializeField] Transform partToPivot;  //used to rotate top
+    [SerializeField] Transform targetEnemy;  //used to look at the enemy
+    
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        partToPivot.LookAt(targetEnemy);
+	}
+}
