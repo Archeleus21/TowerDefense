abbd35e2c152a1927ab7931635ecbe8978b1660e
Roybal_A <Roybal.usmc@gmail.com>
2018-09-27 17:05:19 -0700

    Added towers, shooting, enemies, some effects, need to fix explosion effect

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] ParticleSystem bulletFX;
+    [SerializeField] ParticleSystem deathFX;
+
+    [SerializeField] int health = 5;
+
+    public bool isEnemyAlive;
+
+	// Use this for initialization
+	void Start ()
+    {
+        bulletFX = GetComponentInChildren<ParticleSystem>();
+        deathFX = GetComponentInChildren<ParticleSystem>();
+
+        AddNonTriggerBoxCollider();
+        isEnemyAlive = true;
+	}
+
+    private void AddNonTriggerBoxCollider()
+    {
+        Collider boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.GetComponent<BoxCollider>().size = new Vector3(7, 7, 7);
+        boxCollider.GetComponent<BoxCollider>().center = new Vector3(0, 3.4f, 0);
+        boxCollider.isTrigger = false;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        Death();
+		
+	}
+
+    private void OnParticleCollision(GameObject other)
+    {
+        health--;
+        bulletFX.Play();
+    }
+
+    void DeathExplosion(Vector3 enemyPos)
+    {
+        Instantiate(deathFX, transform.position, Quaternion.identity); //fix explosion
+
+    }
+
+    private void Death()
+    {
+        if(health <= 0)
+        {
+            isEnemyAlive = false;
+            DeathExplosion(transform.position);
+            Destroy(gameObject);
+        }
+        else
+        {
+            isEnemyAlive = true;
+        }
+    }
+
+    public bool GetIsEnemyAlive()
+    {
+        return isEnemyAlive;
+    }
+}
--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -4,14 +4,44 @@ using UnityEngine;
 
 public class Tower : MonoBehaviour
 {
+    GameObject enemy;
+    EnemyHealth enemyHealth;
 
     [SerializeField] Transform partToPivot;  //used to rotate top
     [SerializeField] Transform targetEnemy;  //used to look at the enemy
-    
-	
-	// Update is called once per frame
-	void Update ()
+    [SerializeField] Transform firePoint;  //firing position
+    [SerializeField] ParticleSystem towerBullets; //used to store bullet particle
+
+    float fireRate = .5f;
+    public bool isShooting;
+
+    private void Start()
     {
-        partToPivot.LookAt(targetEnemy);
+        enemy = GameObject.Find("Enemy");  //instance of object reference
+        enemyHealth = enemy.GetComponent<EnemyHealth>();  //gets script from object that was referenced
+        towerBullets = GetComponentInChildren<ParticleSystem>();  //gets particlesystem
+
+        StartCoroutine(towerShooting(enemyHealth.GetIsEnemyAlive()));  //starts coroutine to fire
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        partToPivot.LookAt(targetEnemy);  //used to track the enemy
+        
 	}
+
+    IEnumerator towerShooting(bool isEnemyAlive)  //used to shoot at the enemy as long as the enemy is alive
+    {
+
+        while (isEnemyAlive) //runs while enemy is alive
+        {
+            towerBullets.Play();  //shoots bullets
+            yield return new WaitForSeconds(fireRate);  //pauses to control firing speed
+            towerBullets.Stop();  //stops shooting
+            isEnemyAlive = enemyHealth.GetIsEnemyAlive();  //checks if enemy is still alive
+
+        }
+    }
+
 }
